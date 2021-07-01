2b88df893ef473649124b004009caec6a0a1f15d
datyasuo1 <73332200+datyasuo1@users.noreply.github.com>
2021-07-01 13:32:02 +0700

    Task5

---
+++ CodeSS6/CodeSS6/Sum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ex1
+{
+    class Sum
+    {
+        int Add (int numOne, int numTwo)
+        {
+            int addResult = numOne + numTwo;
+            Console.WriteLine("Addition = " + addResult);
+        }
+        static void Main(string[] args)
+        {
+            Sum objSum = new Sum();
+            objSum.Add();
+        }
+    }
+}
---
+++ CodeSS6/ex2/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ex2
+{
+    class Book
+    {
+        
+        string _bookName;
+        public string Print()
+        {
+            return _bookName;
+        }
+        public void Input(string bkName)
+        {
+            _bookName = bkName;
+        }
+        static void Main(string[] args)
+        {
+            Book objBook = new Book();
+            objBook.Input("C#-The Complete Reference");
+            Console.WriteLine(objBook.Print());
+        }
+    }
+}
+
